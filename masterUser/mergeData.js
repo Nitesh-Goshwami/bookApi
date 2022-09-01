0df838d9cc36db81da2abaed2ccfa8503c1c263d
Nitesh Kumar Goshwami <86410121+Nitesh-Goshwami@users.noreply.github.com>
2022-09-02 00:20:32 +0530

    optimized the solution

--- masterUser/mergeData.js
+++ masterUser/mergeData.js
@@ -1,27 +1,32 @@
 const masterData = require('D:/TurnoutNow/masterUser/user_master.json');
-const secondryData = require('D:/TurnoutNow/masterUser/users_secondary.json');
+const secondaryData = require('D:/TurnoutNow/masterUser/users_secondary.json');
 const config = require('D:/TurnoutNow/masterUser/configure.json');
-
 let result = [];
 
-masterData.forEach((master) => {
-    secondryData.filter((secondary) => {
-        if (master.id === secondary.attendeeId) {
+// Converting the the secondary user array into object.
+let secondaryObj = secondaryData.reduce((arr, value) => ({ ...arr, [value.attendeeId]: value }), {});
+
+
+for (let master of masterData) {
+    for (let secondary in secondaryObj) {       //This loop is looking for the key in object .       
+        if (master.id === secondary) {              // looking up a key in an object is O(1)
             let ans = {}
             ans[config[0].id] = master.id
             ans[config[0].firstName] = master.firstName
             ans[config[0].lastName] = master.lastName
             ans[config[0].attendeeType] = master.attendeeType
             ans[config[0].badgeType] = master.badgeType
-            ans[config[0].zip] = secondary.zip
-            ans[config[0].country] = secondary.country
-            ans[config[0].city] = secondary.city
-            ans[config[0].registered] = secondary.registered
-            ans[config[0].fileURL] = secondary.files.length ? secondary.files[secondary.files.length - 1].fileURL : ""
-            result.push(ans)
+            ans[config[0].zip] = secondaryObj[secondary].zip
+            ans[config[0].country] = secondaryObj[secondary].country
+            ans[config[0].city] = secondaryObj[secondary].city
+            ans[config[0].registered] = secondaryObj[secondary].registered
+            ans[config[0].fileURL] = secondaryObj[secondary].files.length ? secondaryObj[secondary].files[secondaryObj[secondary].files.length - 1].fileURL : ""
+            result.push(ans);
+            break;
         }
-    })
-})
+    }
+}
 
 
 console.log(result)
+
