79918d41980c732f1067ebda97c7098cf86d8980
Nitesh Kumar Goshwami <86410121+Nitesh-Goshwami@users.noreply.github.com>
2022-09-01 21:30:52 +0530

    Adding configration in a ssignment two

--- masterUser/mergeData.js
+++ masterUser/mergeData.js
@@ -1,24 +1,23 @@
 const masterData = require('D:/TurnoutNow/masterUser/user_master.json');
 const secondryData = require('D:/TurnoutNow/masterUser/users_secondary.json');
-const configured = require('D:/TurnoutNow/masterUser/configure.json');
+const config = require('D:/TurnoutNow/masterUser/configure.json');
 
 let result = [];
 
 masterData.forEach((master) => {
     secondryData.filter((secondary) => {
         if (master.id === secondary.attendeeId) {
-            let ans = {
-                attendee_id: master.id,
-                first_name: master.firstName,
-                last_name: master.lastName,
-                attendee_type: master.attendeeType,
-                badge_type: master.badgeType,
-                zip: secondary.zip,
-                country: secondary.country,
-                city: secondary.city,
-                registered: secondary.registered,
-                userImage_url: secondary.files.length ? secondary.files[secondary.files.length - 1].fileURL : "",
-            }
+            let ans = {}
+            ans[config[0].id] = master.id
+            ans[config[0].firstName] = master.firstName
+            ans[config[0].lastName] = master.lastName
+            ans[config[0].attendeeType] = master.attendeeType
+            ans[config[0].badgeType] = master.badgeType
+            ans[config[0].zip] = secondary.zip
+            ans[config[0].country] = secondary.country
+            ans[config[0].city] = secondary.city
+            ans[config[0].registered] = secondary.registered
+            ans[config[0].fileURL] = secondary.files.length ? secondary.files[secondary.files.length - 1].fileURL : ""
             result.push(ans)
         }
     })
