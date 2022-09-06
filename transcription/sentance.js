87f8f5d5e2258de1ce4f370fe86af7ce1a18dd0f
Nitesh Kumar Goshwami <86410121+Nitesh-Goshwami@users.noreply.github.com>
2022-09-06 19:18:02 +0530

    improved code structure

--- transcription/sentance.js
+++ transcription/sentance.js
@@ -1,12 +1,28 @@
 const json = require('./transcriptData.json');
 const data = json.results.items;
 
-let [phrases, result, sentance, phrase, startTime, phraseWords] = [[], [], "", "", 0, 0]
+let [phrases,sentances, result, sentance, phrase, startTime, phraseWords] = [[],[], [], "", "", 0, 0]
 let flag = false;
 
+
 for (let i = 0; i < data.length; i++) {
-    if (data[i].alternatives[0].content != "." && data[i].alternatives[0].content != "?") {
+    if (data[i].alternatives[0].content == "." || data[i].alternatives[0].content == "?") {
+        sentance += data[i].alternatives[0].content;
+        let ans = {
+            start_time: data[startTime].start_time,
+            end_time: data[i - 1].end_time,
+            sentance: sentance
+        }
+        sentances.push(ans)
+        sentance = "";
+        startTime = i + 1;
+        flag = true; // will indicate the end of sentance to prevent the phrase boundry 
+
+    } else {
         sentance += data[i].alternatives[0].content + " ";
+        phrase += data[i].alternatives[0].content + " ";
+        phraseWords++;
+
         if (data[i].alternatives[0].content == ",") {
             phraseWords--;
         }
@@ -17,29 +33,19 @@ for (let i = 0; i < data.length; i++) {
                 phrase: phrase
             }
             phrases.push(temp);
-            flag = false;
             phrase = "";
             phraseWords = 0
         }
-        phrase += data[i].alternatives[0].content + " ";
-        phraseWords++;
-    } else {
-        sentance += data[i].alternatives[0].content;
-        let ans = {
-            start_time: data[startTime].start_time,
-            end_time: data[i - 1].end_time,
-            sentance: sentance
+        if (flag) {
+            let val = {
+                sentances: sentances,
+                phrases: phrases
+            }
+            result.push(val);
+            sentances = [];
+            phrases = [];
+            flag = false;
         }
-        sentance = "";
-        t = i + 1;
-        flag = true; // will indicate the end of sentance to prevent the phrase boundry 
-        let val = {
-            sentance: ans,
-            phrase: phrases
-        }
-        result.push(val);
-        phrases = [];
     }
 }
-
 console.log(result)
