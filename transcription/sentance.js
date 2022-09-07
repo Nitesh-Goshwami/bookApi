659e8c8e8a0545da971f25cb7629ffe7d724da12
Nitesh Kumar Goshwami <86410121+Nitesh-Goshwami@users.noreply.github.com>
2022-09-07 12:56:37 +0530

    corrected the phrase logic

--- transcription/sentance.js
+++ transcription/sentance.js
@@ -1,29 +1,12 @@
 const json = require('./transcriptData.json');
 const data = json.results.items;
 
-let [phrases, sentances, result, sentance, phrase,
-    sentStartTime, pharStartTime, phraseWords] = [[], [], [], "", "", 0, 0, 0]
+let [phrases, sentances, sentance, phrase, startTime, pharStartTime, phraseWords] = [[], [], "", "", 0, 0, 0]
 let flag = false;
 
-
 for (let i = 0; i < data.length; i++) {
-    if (data[i].alternatives[0].content == "." || data[i].alternatives[0].content == "?") {
-        sentance += data[i].alternatives[0].content;
-        let ans = {
-            start_time: data[sentStartTime].start_time,
-            end_time: data[i - 1].end_time,
-            sentance: sentance
-        }
-        sentances.push(ans)
-        sentance = "";
-        sentStartTime = i + 1;
-        flag = true; // will indicate the end of sentance to prevent the phrase boundry 
-
-    } else {
+    if (data[i].alternatives[0].content != "." && data[i].alternatives[0].content != "?") {
         sentance += data[i].alternatives[0].content + " ";
-        phrase += data[i].alternatives[0].content + " ";
-        phraseWords++;
-
         if (data[i].alternatives[0].content == ",") {
             phraseWords--;
         }
@@ -33,21 +16,29 @@ for (let i = 0; i < data.length; i++) {
                 end_time: data[i - 1].end_time,
                 phrase: phrase
             }
-            phrases.push(temp);
-            phrase = "";
-            phraseWords = 0;
-            pharStartTime = i + 1;
-        }
-        if (flag) {
-            let val = {
-                sentances: sentances,
-                phrases: phrases
+            if (flag) {
+                temp.end_time = data[i - 2].end_time
             }
-            result.push(val);
-            sentances = [];
-            phrases = [];
+            phrases.push(temp);
+            pharStartTime = i;
             flag = false;
+            phrase = "";
+            phraseWords = 0
         }
+        phrase += data[i].alternatives[0].content + " ";
+        phraseWords++;
+    } else {
+        sentance += data[i].alternatives[0].content;
+        let ans = {
+            start_time: data[startTime].start_time,
+            end_time: data[i - 1].end_time,
+            sentance: sentance
+        }
+        sentances.push(ans)
+        sentance = "";
+        startTime = i + 1;
+        flag = true;
     }
 }
-console.log(result)
+
+console.log(sentances, phrases)
